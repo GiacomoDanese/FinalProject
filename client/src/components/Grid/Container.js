e7a7c523de6dd6a4aa2edf3764f89038da5a2ed3
Giacomo Danese <Giacomo@ZingTasks-MacBook-Pro.local>
2018-01-12 22:13:49 -0700

    feed

--- client/src/components/Grid/Container.js
+++ client/src/components/Grid/Container.js
@@ -2,6 +2,6 @@ import React from "react";
 import Logo from '../Logo/city.jpeg';
 
 export const Container = ({ fluid, children }) =>
-  <div className={`container${fluid ? "-fluid" : ""}`} style={{padding:"100px", }}>
+  <div className={`container${fluid ? "-fluid" : ""}`} style={{paddingTop:"100px", }}>
     {children}
   </div>;
